5d34ae0f13af2d8f698e2d944f1843909281f373
Richard Bramley <rabramley@gmail.com>
2020-11-20 15:49:20 +0000

    Add templates

--- lbrc_flask/__init__.py
+++ lbrc_flask/__init__.py
@@ -1,9 +1,14 @@
+from flask import Blueprint
 from .emailing import init_mail
 from .standard_views import init_standard_views
 from .template_filters import init_template_filters
 
 
 def init_lbrc_flask(app):
+
+    blueprint = Blueprint("lbrc_flask", __name__, template_folder="templates")
+    app.register_blueprint(blueprint)
+
     init_mail(app)
     init_standard_views(app)
     init_template_filters(app)
