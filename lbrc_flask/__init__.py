3c53327120ae56a7369f8203ddee9e04ea86a3df
Richard Bramley <rabramley@gmail.com>
2020-11-20 15:36:11 +0000

    Ass template filters

--- lbrc_flask/__init__.py
+++ lbrc_flask/__init__.py
@@ -1,7 +1,9 @@
 from .emailing import init_mail
 from .standard_views import init_standard_views
+from .template_filters import init_template_filters
 
 
 def init_lbrc_flask(app):
     init_mail(app)
     init_standard_views(app)
+    init_template_filters(app)
