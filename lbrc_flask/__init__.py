b07ab55eb9513955a37ab4dd075e58b738128b4b
Richard Bramley <rabramley@gmail.com>
2020-11-20 16:23:01 +0000

    Absolute static path

--- lbrc_flask/__init__.py
+++ lbrc_flask/__init__.py
@@ -6,7 +6,7 @@ from .template_filters import init_template_filters
 
 def init_lbrc_flask(app):
 
-    blueprint = Blueprint("lbrc_flask", __name__, template_folder="templates", static_folder='static')
+    blueprint = Blueprint("lbrc_flask", __name__, template_folder="templates", static_folder='static', static_url_path='lbrc_flask/static')
     app.register_blueprint(blueprint)
 
     init_mail(app)
