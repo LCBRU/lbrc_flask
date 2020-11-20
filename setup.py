1276c5e03db24e049c59a29c1156f503d01700c0
Richard Bramley <rabramley@gmail.com>
2020-11-20 14:22:55 +0000

    Module it

--- setup.py
+++ setup.py
@@ -1,26 +1,12 @@
 #!/usr/bin/env python
-import os
-import sys
-from itertools import chain
 
-from setuptools import setup
-from setuptools.config import read_configuration
+from distutils.core import setup
 
-# Append cwd for pip 19
-sys.path.append(os.path.abspath("."))
-
-################################################################################
-# Programmatically generate some extras combos.
-################################################################################
-extras = read_configuration("setup.cfg")["options"]["extras_require"]
-
-# Dev is everything
-extras["dev"] = list(chain(*extras.values()))
-
-# All is everything but tests and docs
-exclude_keys = ("tests", "docs", "dev")
-ex_extras = dict(filter(lambda i: i[0] not in exclude_keys, extras.items()))
-# Concatenate all the values together for 'all'
-extras["all"] = list(chain.from_iterable(ex_extras.values()))
-
-setup(extras_require=extras, use_scm_version=True, setup_requires=['setuptools_scm'])
+setup(name='lbrc_flask_ui',
+      version='1.0',
+      description='NIHR Leicester BRC Flask UI Components and Theme',
+      author='Richard Bramley',
+      author_email='rabramley@gmail.com',
+      url='https://github.com/LCBRU/lbrc_flask_ui/',
+      packages=['lbrc_flask_ui'],
+     )
