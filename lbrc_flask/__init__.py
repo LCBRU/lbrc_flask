a0c6b7062030438cfc26d7ad4bfd8eea5aa27ad4
Richard Bramley <rabramley@gmail.com>
2020-11-23 14:49:17 +0000

    Database is common

--- lbrc_flask/__init__.py
+++ lbrc_flask/__init__.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, g
+from flask_sqlalchemy import SQLAlchemy
 from .emailing import init_mail
 from .standard_views import init_standard_views
 from .template_filters import init_template_filters
 from .admin import init_admin
+
+
+db = SQLAlchemy()
 
 
 def init_lbrc_flask(app, title):
@@ -10,6 +14,7 @@ def init_lbrc_flask(app, title):
     blueprint = Blueprint("lbrc_flask", __name__, template_folder="templates", static_folder='static', url_prefix='/lbrc_flask')
     app.register_blueprint(blueprint)
 
+    db.init_app(app)
     init_mail(app)
     init_standard_views(app)
     init_template_filters(app)
