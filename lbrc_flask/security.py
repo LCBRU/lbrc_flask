5d9833ab439c84db2acb705bff56a15cb5f3bd76
Richard Bramley <rabramley@gmail.com>
2020-11-24 18:49:08 +0000

    Defining user and role centrall doesnt work

--- lbrc_flask/security.py
+++ lbrc_flask/security.py
@@ -1,6 +1,5 @@
 import random
 import string
-from datetime import datetime
 from flask_security import Security, SQLAlchemyUserDatastore
 from flask_security.forms import (
     EqualTo,
@@ -13,29 +12,9 @@ from flask_security.forms import (
 )
 from flask_security.utils import verify_and_update_password, get_message
 from flask_login import current_user
-from flask_security import UserMixin, RoleMixin
 from wtforms.validators import ValidationError
 from wtforms import PasswordField, SubmitField
 from .database import db
-
-
-class Role(db.Model, RoleMixin):
-    ADMIN_ROLENAME = "admin"
-
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
-    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    def __str__(self):
-        return self.name or ""
-
-
-roles_users = db.Table(
-    "roles_users",
-    db.Column("user_id", db.Integer(), db.ForeignKey("user.id")),
-    db.Column("role_id", db.Integer(), db.ForeignKey("role.id")),
-)
 
 
 def random_password():
@@ -48,38 +27,6 @@ def random_password():
         )
         for _ in range(15)
     )
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255), nullable=False, default=random_password)
-    first_name = db.Column(db.String(255))
-    last_name = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
-    confirmed_at = db.Column(db.DateTime())
-    last_login_at = db.Column(db.DateTime())
-    current_login_at = db.Column(db.DateTime())
-    last_login_ip = db.Column(db.String(50))
-    current_login_ip = db.Column(db.String(50))
-    login_count = db.Column(db.Integer())
-    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    roles = db.relationship(
-        "Role", secondary=roles_users, backref=db.backref("users", lazy="dynamic")
-    )
-
-    @property
-    def is_admin(self):
-        return self.has_role(Role.ADMIN_ROLENAME)
-
-    @property
-    def full_name(self):
-        full_name = " ".join(filter(None, [self.first_name, self.last_name]))
-
-        return full_name or self.email
-
-    def __str__(self):
-        return self.email
 
 
 class PasswordPolicy(ValidatorMixin):
@@ -155,7 +102,7 @@ class ChangePasswordForm(Form, PasswordFormMixin):
         return True
 
 
-def init_security(app, user_class=User, role_class=Role):
+def init_security(app, user_class, role_class):
     user_datastore = SQLAlchemyUserDatastore(db, user_class, role_class)
     Security(
         app,
@@ -167,7 +114,7 @@ def init_security(app, user_class=User, role_class=Role):
     @app.before_first_request
     def init_security():
         admin_role = user_datastore.find_or_create_role(
-            name=Role.ADMIN_ROLENAME, description=Role.ADMIN_ROLENAME
+            name=role_class.ADMIN_ROLENAME, description=role_class.ADMIN_ROLENAME
         )
 
         for a in app.config["ADMIN_EMAIL_ADDRESSES"].split(";"):
