75de34437453bcc2ac8edbd6cc972af0a1ebb09d
Richard Bramley <rabramley@gmail.com>
2020-11-26 09:15:24 +0000

    Add form builder

--- lbrc_flask/forms.py
+++ lbrc_flask/forms.py
@@ -5,7 +5,8 @@ from wtforms import (
     StringField,
     HiddenField,
 )
-from wtforms.validators import Length, DataRequired
+from wtforms.validators import Length, DataRequired, Optional
+from flask_wtf.file import FileAllowed
 
 
 class FlashingForm(FlaskForm):
@@ -31,3 +32,50 @@ class SearchForm(FlashingForm):
 
 class ConfirmForm(FlashingForm):
     id = HiddenField("id", validators=[DataRequired()])
+
+
+class FormBuilder:
+
+    def __init__(self):
+        self._fields = {}
+
+    def get_form(self):
+        class DynamicForm(FlashingForm):
+            pass
+
+        for name, field in self._fields.items():
+            setattr(DynamicForm, name, field)
+
+        return DynamicForm()
+
+    def add_field(self, field):
+        form_field = None
+
+        kwargs = {"validators": [], "default": field.get_default()}
+
+        if field.required:
+            kwargs["validators"].append(DataRequired())
+        else:
+            kwargs["validators"].append(Optional())
+
+        if field.max_length:
+            kwargs["validators"].append(Length(max=field.max_length))
+
+        if field.choices:
+            kwargs["choices"] = field.get_choices()
+
+        if field.allowed_file_extensions:
+            kwargs["validators"].append(
+                FileAllowed(
+                    field.get_allowed_file_extensions(),
+                    'Only the following file extensions are allowed "{}".'.format(
+                        ", ".join(field.get_allowed_file_extensions())
+                    ),
+                )
+            )
+
+        module = __import__("wtforms")
+        class_ = getattr(module, field.field_type.name)
+        form_field = class_(field.field_name, **kwargs)
+
+        self._fields[field.field_name] = form_field
