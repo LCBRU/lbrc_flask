3e4e153808fa8398e5ccdfe4fdf0deba5a95142c
Richard Bramley <rabramley@gmail.com>
2021-01-14 11:12:26 +0000

    Modal macros

--- lbrc_flask/static/js/lbrc_flask.js
+++ lbrc_flask/static/js/lbrc_flask.js
@@ -5,9 +5,13 @@ function standard_status_actions(response) {
     } else if (response.status >= 200 && response.status < 300) {
         return response
     } else {
-        var error = new Error(response.statusText)
-        error.response = response
-        throw error
+        response.text().then(function (text) {
+            modal = $('#errorModal')
+            modal.find('#error_status').text(response.status);
+            modal.find('#error_status_text').text(response.statusText);
+            modal.find('#error_text').text(text);
+            modal.modal('show');
+        });
     }
 }
 
