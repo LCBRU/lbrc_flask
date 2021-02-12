a19563e498362d9455227e402515d4b38b76fb77
Richard Bramley <rabramley@gmail.com>
2021-02-12 12:37:15 +0000

    Security login form

--- lbrc_flask/security/__init__.py
+++ lbrc_flask/security/__init__.py
@@ -1,18 +1,6 @@
-import string
+from lbrc_flask.security.forms import LbrcChangePasswordForm, LbrcLoginForm, LbrcResetPasswordForm
 from flask_security import Security, SQLAlchemyUserDatastore
-from flask_security.forms import (
-    EqualTo,
-    password_length,
-    password_required,
-    get_form_field_label,
-    ValidatorMixin,
-    Form,
-    PasswordFormMixin,
-)
-from flask_security.utils import verify_and_update_password, get_message
 from flask_login import current_user
-from wtforms.validators import ValidationError
-from wtforms import PasswordField, SubmitField
 from ..database import db
 from .model import User, Role, AuditMixin
 
@@ -21,86 +9,14 @@ def current_user_id():
     return current_user.id
 
 
-class PasswordPolicy(ValidatorMixin):
-    def __init__(
-        self,
-        message=u"The password must contain a lowercase, "
-        "uppercase and punctuation character",
-    ):
-        self.message = message
-
-    def __call__(self, form, field):
-        value = set(field.data)
-
-        if (
-            value.isdisjoint(string.ascii_lowercase)
-            or value.isdisjoint(string.ascii_uppercase)
-            or value.isdisjoint(string.punctuation)
-        ):
-            raise ValidationError(self.message)
-
-
-class NewPasswordFormMixin:
-    password = PasswordField(
-        get_form_field_label("password"),
-        validators=[password_required, password_length, PasswordPolicy()],
-    )
-
-
-class PasswordConfirmFormMixin:
-    password_confirm = PasswordField(
-        get_form_field_label("retype_password"),
-        validators=[
-            EqualTo("password", message="RETYPE_PASSWORD_MISMATCH"),
-            password_required,
-        ],
-    )
-
-
-class ResetPasswordForm(Form, NewPasswordFormMixin, PasswordConfirmFormMixin):
-    """The default reset password form"""
-
-    submit = SubmitField(get_form_field_label("reset_password"))
-
-
-class ChangePasswordForm(Form, PasswordFormMixin):
-    """The default change password form"""
-
-    new_password = PasswordField(
-        get_form_field_label("new_password"),
-        validators=[password_required, password_length, PasswordPolicy()],
-    )
-
-    new_password_confirm = PasswordField(
-        get_form_field_label("retype_password"),
-        validators=[
-            EqualTo("new_password", message="RETYPE_PASSWORD_MISMATCH"),
-            password_required,
-        ],
-    )
-
-    submit = SubmitField(get_form_field_label("change_password"))
-
-    def validate(self):
-        if not super(ChangePasswordForm, self).validate():
-            return False
-
-        if not verify_and_update_password(self.password.data, current_user):
-            self.password.errors.append(get_message("INVALID_PASSWORD")[0])
-            return False
-        if self.password.data.strip() == self.new_password.data.strip():
-            self.password.errors.append(get_message("PASSWORD_IS_THE_SAME")[0])
-            return False
-        return True
-
-
 def init_security(app, user_class, role_class):
     user_datastore = SQLAlchemyUserDatastore(db, user_class, role_class)
     Security(
         app,
         user_datastore,
-        reset_password_form=ResetPasswordForm,
-        change_password_form=ChangePasswordForm,
+        reset_password_form=LbrcResetPasswordForm,
+        change_password_form=LbrcChangePasswordForm,
+        login_form=LbrcLoginForm,
     )
 
     @app.before_first_request
