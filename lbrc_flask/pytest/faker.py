6efe38602ddc35171baab9278d33692f74be071f
Richard Bramley <rabramley@gmail.com>
2021-03-01 14:15:08 +0000

    Add faker person details

--- lbrc_flask/pytest/faker.py
+++ lbrc_flask/pytest/faker.py
@@ -1,8 +1,14 @@
-import re
+import datetime
+from dateutil.relativedelta import relativedelta
 from faker.providers import BaseProvider
 from lbrc_flask.security import Role, User
 from lbrc_flask.forms.dynamic import FieldGroup, Field, FieldType
 from lbrc_flask.database import db
+from random import randint, choice, randrange
+from lbrc_flask.validators import (
+    is_invalid_nhs_number,
+    calculate_nhs_number_checksum,
+)
 
 
 class LbrcFlaskFakerProvider(BaseProvider):
@@ -25,6 +31,87 @@ class LbrcFlaskFakerProvider(BaseProvider):
         db.session.commit()
 
         return user
+
+    def nhs_number(self):
+        while True:  
+            number = str(randint(100_000_000, 999_999_999))
+            whole_num = f'{number}{calculate_nhs_number_checksum(number)}'
+
+            if(not is_invalid_nhs_number(whole_num)):  
+                return whole_num
+
+    def invalid_nhs_number(self):
+        return 'ABC'
+
+    def uhl_system_number(self):
+        prefix = choice(['S', 'R', 'F', 'G', 'U', 'LB', 'RTD'])
+        return f'{prefix}{randint(1_000_000, 9_999_999)}'
+
+    def invalid_uhl_system_number(self):
+        return 'ABC'
+
+    def gp_practice_code(self):
+        prefix = choice(['ABCDEFGHIJKLMNOPQRSTUVWXYZ'])
+        return f'{prefix}{randint(10_000, 99_999)}'
+
+    def person_details(self):
+        if not randint(0, 1):
+            return self.female_person_details()
+        else:
+            return self.male_person_details()
+
+    def female_person_details(self):
+        return {
+            **{
+                'family_name': self.generator.last_name_female(),
+                'given_name': self.generator.first_name_female(),
+                'middle_name': self.generator.first_name_female(),
+                'gender': 'F',
+                'title': self.generator.prefix_female(),
+            },
+            **self._generic_person_details(),
+        }
+
+    def male_person_details(self):
+        return {
+            **{
+                'family_name': self.generator.last_name_male(),
+                'given_name': self.generator.first_name_male(),
+                'middle_name': self.generator.first_name_male(),
+                'gender': 'M',
+                'title': self.generator.prefix_male(),
+            },
+            **self._generic_person_details(),
+        }
+
+    def _generic_person_details(self):
+
+        today = datetime.date.today()
+        dob = self._random_date(today - relativedelta(years=75), today - relativedelta(years=40))
+
+        if randint(0, 10):
+            dod = None
+            is_deceased = False
+        else:
+            dod = self._random_date(dob, today)
+            is_deceased = True
+
+        return {
+            'date_of_birth': dob,
+            'date_of_death': dod,
+            'is_deceased': is_deceased,
+            'address': self.generator.address(),
+            'current_gp_practice_code': self.gp_practice_code(),
+            'nhs_number': self.generator.nhs_number(),
+            'uhl_system_number': self.generator.uhl_system_number(),
+            'postcode': self.generator.postcode(),
+        }
+    
+    def _random_date(self, start_date, end_date):
+        time_between_dates = end_date - start_date
+        days_between_dates = time_between_dates.days
+        random_number_of_days = randrange(days_between_dates)
+        return start_date + datetime.timedelta(days=random_number_of_days)
 
 
 class LbrcDynaicFormFakerProvider(BaseProvider):
