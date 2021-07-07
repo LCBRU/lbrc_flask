f06fd0298808d2dc5af467dacb845ab289a2a479
Richard Bramley <rabramley@gmail.com>
2021-07-07 16:47:37 +0000

    Search forms use standard control rendering

--- lbrc_flask/forms/__init__.py
+++ lbrc_flask/forms/__init__.py
@@ -9,6 +9,8 @@ from wtforms import (
 )
 from wtforms.validators import Length, DataRequired
 from wtforms.widgets import html_params
+from flask_wtf.file import FileField as _FileField
+from wtforms.widgets import FileInput as _FileInput
 
 
 class DescriptionField(Field):
@@ -50,3 +52,21 @@ class SearchForm(FlashingForm):
 
 class ConfirmForm(FlashingForm):
     id = HiddenField("id", validators=[DataRequired()])
+
+
+class FileInput(_FileInput):
+
+    def __call__(self, field, **kwargs):
+        if field.accept:
+            kwargs[u'accept'] = ','.join(field.accept)
+        return _FileInput.__call__(self, field, **kwargs)
+
+
+class FileField(_FileField):
+    widget = FileInput()
+
+    def __init__(self, *args, **kwargs):
+        self.accept = kwargs.pop('accept', None)
+        super(FileField, self).__init__(*args, **kwargs)
+
+
