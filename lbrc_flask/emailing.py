e7f3747d9f37a72033126065d750da1a7f4cfb1b
Richard Bramley <rabramley@gmail.com>
2021-11-22 16:39:05 +0000

    Add REPLY TO address

--- lbrc_flask/emailing.py
+++ lbrc_flask/emailing.py
@@ -11,7 +11,13 @@ def init_mail(app):
 
 def email(subject, message, recipients, html_template=None, **kwargs):
     if current_app.config["SMTP_SERVER"] is not None:
-        msg = Message(subject=subject, recipients=recipients, body=message)
+
+        msg = Message(
+            subject=subject,
+            recipients=recipients,
+            body=message,
+            reply_to=current_app.config.get("MAIL_REPLY_TO", None),
+        )
 
         if html_template is not None:
             msg.html = render_template(html_template, **kwargs)
