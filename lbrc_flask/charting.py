371fa8986691c25d40812e732cb90c1181b42b90
Richard Bramley <rabramley@gmail.com>
2023-08-02 14:57:51 +0100

    Add easier bar charts

--- lbrc_flask/charting.py
+++ lbrc_flask/charting.py
@@ -1,7 +1,62 @@
-import pygal
-from itertools import groupby
-from pygal.style import Style
 from collections import Counter
+from dataclasses import dataclass
+from datetime import datetime
+from itertools import groupby
+from tempfile import NamedTemporaryFile
+
+import pygal
+from flask import send_file
+from pygal.style import Style
+
+
+@dataclass
+class BarChartItem:
+    series: str
+    bucket: str
+
+
+class BarChart:
+    def __init__(self, title: str, items: list[BarChartItem], buckets: list[str]=None) -> None:
+        self.title: str = title
+        self.items: list[BarChartItem] = sorted(items, key=lambda i: i.series)
+        self.buckets: list[str] = buckets or sorted({i.bucket for i in self.items})
+        self.series: list[str] = sorted({i.series for i in self.items})
+
+    def get_chart(self):
+        chart = pygal.Bar()
+        chart.show_minor_y_labels = False
+        chart.style = Style(font_family='Lato')
+        chart.title = self.title
+        chart.x_labels = self.buckets
+
+        max_y_label = 0
+
+        for series_name, series_items in groupby(self.items, lambda i: i.series):
+            count = Counter([i.bucket for i in series_items])
+            values = {b: count.get(b, 0) for b in self.buckets}
+
+            max_y_label = max([max_y_label] + list(values.values()))
+
+            chart.add(series_name, values.values())
+
+        if max_y_label > 0:
+            chart.y_labels_major_count = max_y_label + 1
+
+        return chart
+
+    def send_as_attachment(self):
+        chart = self.get_chart()
+
+        with NamedTemporaryFile() as tmp:
+            chart.render_to_png(tmp.name)
+
+            return send_file(
+                tmp.name,
+                as_attachment=True,
+                download_name=f'{self.title}_{datetime.utcnow():%Y%m%d_%H%M%S}.png',
+                max_age=0,
+                mimetype='image/png',
+            )
 
 
 def grouped_bar_chart(title, details, buckets=None):
@@ -9,7 +64,7 @@ def grouped_bar_chart(title, details, buckets=None):
     details = sorted(details, key=lambda d: d['group'])
 
     if buckets is None:
-        buckets = {d['category'] for d in details}
+        buckets = sorted({d['category'] for d in details})
 
     chart = pygal.Bar()
     chart.show_minor_y_labels = False
@@ -31,3 +86,5 @@ def grouped_bar_chart(title, details, buckets=None):
         chart.y_labels_major_count = max_y_label + 1
 
     return chart
+
+
