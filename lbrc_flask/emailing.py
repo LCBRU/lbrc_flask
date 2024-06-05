bd13da874880df9386ae2ae55bd6e37b390a1c7a
Richard Bramley <rabramley@gmail.com>
2024-06-05 10:00:16 +0100

    Doing nothing

--- lbrc_flask/emailing.py
+++ lbrc_flask/emailing.py
@@ -26,9 +26,6 @@ def email(subject, message, recipients, html_template=None, **kwargs):
         if html_template is not None:
             msg.html = render_template(html_template, **kwargs)
 
-        print(message)
-        print(msg.html)
-
         current_app.logger.info('Sending from to {}'.format(current_app.config.get("MAIL_DEFAULT_SENDER", None)))
         current_app.logger.info('With reply to of {}'.format(current_app.config.get("MAIL_REPLY_TO", None)))
         current_app.logger.info('Sending email to {}'.format(recipients))
