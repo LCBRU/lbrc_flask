455f076136ee193647429ba3edf9356e373994c2
Richard Bramley <rabramley@gmail.com>
2024-06-17 15:49:56 +0100

    Trigger response

--- lbrc_flask/response.py
+++ lbrc_flask/response.py
@@ -5,3 +5,9 @@ def refresh_response():
     resp = Response("Refresh")
     resp.headers['HX-Refresh'] = 'true'
     return resp
+
+
+def trigger_response(trigger_name):
+    resp = Response(trigger_name)
+    resp.headers['HX-Trigger'] = trigger_name
+    return resp
