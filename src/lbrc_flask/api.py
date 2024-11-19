5a2aa79dcc3b04331a41545717454b3d32d8e69f
Richard Bramley <rabramley@gmail.com>
2024-11-19 07:44:33 +0000

    Login API calls as system user

--- src/lbrc_flask/api.py
+++ src/lbrc_flask/api.py
@@ -2,6 +2,8 @@ from functools import wraps
 import uuid
 from lbrc_flask.database import db, GUID
 from flask import request, abort
+from flask_login import login_user
+from lbrc_flask.security import get_system_user
 
 
 def get_api_key():
@@ -31,6 +33,8 @@ def validate_api_key():
             print(api_key)
             if not api_key:
                 abort(403)
+
+            login_user(get_system_user())
 
             return f(*args, **kwargs)
 
