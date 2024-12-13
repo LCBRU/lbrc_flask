aeec6fd0f6eb3fdc067d2a1ff3a68a146b6e1a49
Richard Bramley <rabramley@gmail.com>
2024-12-13 15:51:31 +0000

    Multiple checkbox fields

--- src/lbrc_flask/forms/__init__.py
+++ src/lbrc_flask/forms/__init__.py
@@ -224,17 +224,6 @@ class Unique(object):
             raise ValidationError(self.message)
 
 
-class MultiCheckboxField(SelectMultipleField):
-    """
-    A multiple-select, except displays a list of checkboxes.
-
-    Iterating the field will produce subfields, allowing custom rendering of
-    the enclosed checkbox fields.
-    """
-    widget = ListWidget(prefix_label=False)
-    option_widget = CheckboxInput()
-
-
 def boolean_coerce(value):
     value = str(value).lower()
 
