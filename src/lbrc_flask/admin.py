b140d058cbc98f6467730ecb4cf3a33c2bfd40ce
Richard Bramley <rabramley@gmail.com>
2024-12-17 17:17:40 +0000

    Fix is_admin not existing for anonymous users

--- src/lbrc_flask/admin.py
+++ src/lbrc_flask/admin.py
@@ -1,15 +1,29 @@
 import flask_admin as admin
-from flask import current_app
+from flask import current_app, abort, redirect, url_for, request
 from flask_security import current_user
 from flask_admin.contrib.sqla import ModelView
 
 
 class AdminCustomView(ModelView):
     def is_accessible(self):
-        if hasattr(current_user, 'is_admin'):
-            return current_user.is_admin
-        else:
-            return False
+        return (
+            current_user.is_active
+            and current_user.is_authenticated
+            and current_user.has_role("superuser")
+        )
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not
+        accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for("security.login", next=request.url))
 
 
 def init_admin(app, title, views, url=None, endpoint=None):
