15f8957029371a2f05f06da8b9c9befff00078af
Richard Bramley <rabramley@gmail.com>
2024-12-18 08:50:59 +0000

    Email with attachments, no less

--- src/lbrc_flask/emailing.py
+++ src/lbrc_flask/emailing.py
@@ -1,3 +1,5 @@
+from mimetypes import guess_type
+from pathlib import Path
 from flask_mail import Mail, Message
 from flask import current_app, render_template
 
@@ -9,14 +11,16 @@ def init_mail(app):
     mail.init_app(app)
 
 
-def email(subject, message, recipients, html_template=None, **kwargs):
+def email(subject, message, recipients, html_template=None, attachements: list[Path]=None, **kwargs):
+    attachements = attachements or []
+
     if len(recipients) < 1:
         current_app.logger.info('Skipping email with no recipients')
         return
 
     if current_app.config["SMTP_SERVER"] is not None:
 
-        msg = Message(
+        msg: Message = Message(
             subject=subject,
             recipients=recipients,
             body=message,
@@ -25,6 +29,10 @@ def email(subject, message, recipients, html_template=None, **kwargs):
 
         if html_template is not None:
             msg.html = render_template(html_template, **kwargs)
+
+        for a in attachements:
+            absolute_path = Path(current_app.root_path) / a
+            msg.attach(absolute_path.name, guess_type(absolute_path)[0], absolute_path.read_bytes())
 
         current_app.logger.info('Sending from to {}'.format(current_app.config.get("MAIL_DEFAULT_SENDER", None)))
         current_app.logger.info('With reply to of {}'.format(current_app.config.get("MAIL_REPLY_TO", None)))
