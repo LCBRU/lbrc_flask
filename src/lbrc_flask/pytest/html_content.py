56a219b58cc883f47752b92f5d8db82ba0240767
Richard Bramley <rabramley@gmail.com>
2025-03-22 16:26:26 +0000

    Panel List count

--- src/lbrc_flask/pytest/html_content.py
+++ src/lbrc_flask/pytest/html_content.py
@@ -5,15 +5,22 @@ from lbrc_flask.validators import is_integer
 def get_records_found(soup):
     pagination_summary = soup.find(class_="pagination_summary")
 
-    count = re.findall("(\d+) found", pagination_summary.get_text())
+    count = re.findall(r"(\d+) found", pagination_summary.get_text())
     
     if count:
         if is_integer(count[0]):
             return int(count[0])
 
 
-def get_table_rows_found(soup):
+def get_table_row_count(soup):
     tbody = soup.find_all('tbody')[0]
     trs = tbody.find_all('tr')
 
     return len(trs)
+
+
+def get_panel_list_row_count(soup):
+    panel_list = soup.find_all(class_='panel_list')[0]
+    lis = panel_list.find_all('li')
+
+    return len(lis)
