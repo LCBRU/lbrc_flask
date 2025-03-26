b69cd20381d60d190c77d285bcc91a404baac435
Richard Bramley <rabramley@gmail.com>
2025-03-26 15:11:05 +0000

    Switch LDAP to use ldap3 library

--- src/lbrc_flask/security/ldap.py
+++ src/lbrc_flask/security/ldap.py
@@ -1,81 +1,97 @@
 from lbrc_flask.security.model import random_password
-from ldap import initialize, SCOPE_SUBTREE, LDAPError, OPT_REFERRALS
 import traceback
+import ssl
 from flask import current_app
 from lbrc_flask.database import db
 from flask_security.utils import _datastore
+from ldap3.core.exceptions import LDAPException
+from ldap3 import ALL, Connection, Server, Tls, SUBTREE
 
 
 class Ldap():
     def __init__(self):
-        print('What the')
-        self.ldap = None
+        self.connection = None
+        self.URI = (current_app.config.get('LDAP_URI', None) or '').strip()
+        self.BIND_WHO_FORMAT = (current_app.config.get('LDAP_BIND_WHO_FORMAT', None) or '').strip()
+        self.BASEDN = (current_app.config.get('LDAP_BASEDN', None) or '').strip()
+        self.NOPRIV_USER = (current_app.config.get('LDAP_USER', None) or '').strip(),
+        self.NONPRIV_PASSWORD = (current_app.config.get('LDAP_PASSWORD', None) or '').strip(),
+        self.USERNAME_SEARCH_FORMAT = (current_app.config.get('LDAP_SEARCH_FORMAT', None) or '').strip()
+
+        self.FIELDNAME_FULLNAME = (current_app.config.get('LDAP_FIELDNAME_FULLNAME', None) or '').strip()
+        self.FIELDNAME_USERID = (current_app.config.get('LDAP_FIELDNAME_USERID', None) or '').strip()
+        self.FIELDNAME_EMAIL = (current_app.config.get('LDAP_FIELDNAME_EMAIL', None) or '').strip()
+        self.FIELDNAME_GIVEN_NAME = (current_app.config.get('LDAP_FIELDNAME_GIVENNAME', None) or '').strip()
+        self.FIELDNAME_SURNAME = (current_app.config.get('LDAP_FIELDNAME_SURNAME', None) or '').strip()
 
     def is_enabled(self):
-        return len((current_app.config.get('LDAP_URI', None) or '').strip()) > 0 and current_app.config.get('TESTING', False) == False
+        ldap_uri_set = len(self.URI) > 0
+        not_testing = not current_app.config.get('TESTING', False)
+        return ldap_uri_set and not_testing
 
     def login(self, username, password):
-        if not (username or '').strip() or not (password or '').strip():
-            return False
-
-        print('A'*100)
-        print(username)
-
         username = (username or '').strip()
         password = (password or '').strip()
 
-        current_app.logger.info('Attempting login for {}'.format(username))
-        current_app.logger.info('With password {}'.format(password))
+        if not username:
+            current_app.logger.info("Login Invalid: Username empty")
+            return False
+        if not password:
+            current_app.logger.info("Login Invalid: Password empty")
+            return False
 
+        current_app.logger.info(f"Attempting login for '{username}'")
+
+        bind_user_details = self.BIND_WHO_FORMAT.format(
+            username=username,
+            basedn=self.BASEDN,
+        )
+
+        if self._login_bind_user(bind_user_details, password):
+            current_app.logger.info(f"LDAP login Success for '{username}'")
+            return True
+        else:
+            current_app.logger.info(f"LDAP login Failed for '{username}'")
+            return False
+
+
+    def _login_bind_user(self, bind_user, password):
         try:
-            self.ldap = initialize(current_app.config.get('LDAP_URI', None))
-            self.ldap.protocol_version = 3
-            self.ldap.set_option(OPT_REFERRALS, 0)
-
-            who = current_app.config.get('LDAP_BIND_WHO_FORMAT', None).format(
-                username=username,
-                basedn=current_app.config.get('LDAP_BASEDN', None),
+            tls = Tls(version=ssl.PROTOCOL_TLS, ciphers='ALL')
+            server = Server(
+                host="ldap.rcs.le.ac.uk",
+                port=636,
+                use_ssl=True,
+                tls=tls,
+                get_info=ALL,
             )
 
-            print(who)
-
-            self.ldap.simple_bind_s(who, password)
-
-            print('LDAP login Success for {}'.format(username))
-
-            current_app.logger.info('LDAP login Success for {}'.format(username))
+            self.connection = Connection(
+                server,
+                user=bind_user,
+                password=password,
+                auto_bind=True,
+                auto_referrals=False,
+            )
 
             return True
 
-        except LDAPError as e:
-            self.ldap = None
+        except LDAPException as e:
             print(e)
             current_app.logger.error(e)
             return False
 
     def login_nonpriv(self):
-        self.ldap = initialize(current_app.config.get('LDAP_URI', None))
-        self.ldap.protocol_version = 3
-        self.ldap.set_option(OPT_REFERRALS, 0)
-
-        try:
-            self.ldap.simple_bind_s(
-                current_app.config.get('LDAP_USER', None),
-                current_app.config.get('LDAP_PASSWORD', None),
-            )
-
-            current_app.logger.info('No Priv LDAP login Success')
-
+        if self._login_bind_user(self.NOPRIV_USER, self.NONPRIV_PASSWORD):
+            current_app.logger.info("LDAP login Success Non-Privileged user")
             return True
-
-        except LDAPError as e:
-            self.ldap = None
-            print(e)
-            current_app.logger.error(e)
+        else:
+            current_app.logger.info("LDAP login Failed for Non-Privileged user")
             return False
-
+    
     def search_username(self, username):
-        users = self.search(current_app.config.get('LDAP_SEARCH_FORMAT', None).format(username=username))
+        q = self.USERNAME_SEARCH_FORMAT.format(username=username)
+        users = self.search(q)
 
         if len(users) != 1:
             return None
@@ -83,43 +99,45 @@ class Ldap():
         return users[0]
 
     def search_user(self, search_string):
-        return self.search('(|({}=*{}*)({}={}*))'.format(
-            current_app.config.get('LDAP_FIELDNAME_FULLNAME', None),
-            search_string.strip().replace(' ', '*'),
-            current_app.config.get('LDAP_FIELDNAME_USERID', None),
-            search_string,
-        ))
+        search_string = search_string.strip()
+        wildcard_search_string = search_string.strip().replace(' ', '*')
 
-    def search(self, search_string):
-        current_app.logger.info('Searching LDAP for {}'.format(search_string))
+        q = f"(|({self.FIELDNAME_FULLNAME}=*{wildcard_search_string}*)({self.FIELDNAME_USERID}={search_string}*))"
+
+        return self.search(q)
+
+    def search(self, search_query):
+        current_app.logger.info(f"Searching LDAP for {search_query}")
         result = []
 
         try:
-            search_result = self.ldap.search_s(
-                current_app.config.get('LDAP_BASEDN', None),
-                SCOPE_SUBTREE,
-                search_string,
+            self.conn.search(
+                self.BASEDN,
+                search_query,
+                scope=SUBTREE,
+                attributes=[
+                    self.FIELDNAME_USERID,
+                    self.FIELDNAME_FULLNAME,
+                    self.FIELDNAME_EMAIL,
+                    self.FIELDNAME_GIVEN_NAME,
+                    self.FIELDNAME_SURNAME,
+                ]
             )
 
-            for u in search_result:
-                if isinstance(u[1], dict):
-                    current_app.logger.info('LDAP found {}'.format(u))
-                    result.append({
-                        'cn': u[1]['cn'][0].decode("utf-8"),
-                        'username': u[1][current_app.config.get('LDAP_FIELDNAME_USERID', None)][0].decode("utf-8"),
-                        'email': u[1][current_app.config.get('LDAP_FIELDNAME_EMAIL', None)][0].decode("utf-8"),
-                        'given_name': u[1][current_app.config.get('LDAP_FIELDNAME_GIVENNAME', None)][0].decode("utf-8"),
-                        'surname': u[1][current_app.config.get('LDAP_FIELDNAME_SURNAME', None)][0].decode("utf-8"),
-                    })
+            for user in self.connection.entries:
+                current_app.logger.info(f"LDAP found {user}")
+                result.append({
+                    'cn': user[self.FIELDNAME_FULLNAME],
+                    'username': user[self.FIELDNAME_USERID],
+                    'email': user[self.FIELDNAME_EMAIL],
+                    'given_name': user[self.FIELDNAME_GIVEN_NAME],
+                    'surname': user[self.FIELDNAME_SURNAME],
+                })
 
-            # current_app.logger.info('LDAP found {}'.format(result))
-
-        except LDAPError as e:
-            print(traceback.format_exc())
+        except Exception as e:
             current_app.logger.error(traceback.format_exc())
-        
-        finally:
-            return result
+
+        return result
 
 def get_or_create_ldap_user(username):
     ldap = Ldap()
@@ -135,12 +153,9 @@ def get_or_create_ldap_user(username):
             user = _datastore.find_user(email=ldap_user['email']) or _datastore.find_user(username=ldap_user['username'])
 
             if not user:
-                print('A')
                 if current_app.config.get('LDAP_REQUIRE_EXISTING_USER', False):
-                    print('B')
                     return None
 
-                print('A')
                 user = _datastore.create_user(
                     email=ldap_user['email'],
                     password=random_password(),
@@ -154,9 +169,6 @@ def get_or_create_ldap_user(username):
             
             db.session.add(user)
             db.session.commit()
-
-            print(ldap_user['email'])
-            print(username)
 
             return _datastore.find_user(email=ldap_user['email'])
 
